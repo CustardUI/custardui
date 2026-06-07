c2a18efdf0c6e9dedcbb9dde8482a7336bc9bdfd
gerteck <changerteck@gmail.com>
2026-06-07 12:20:35 +0800

    Add resolution of path to vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { readFileSync } from 'fs';
+import path from 'path';
 
 const pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
 
@@ -13,6 +14,13 @@ export default defineConfig({
     tsconfigPaths({ projects: ['./tsconfig.test.json'] }),
     svelte({ hot: !process.env.VITEST }),
   ],
+  resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib'),
+      $features: path.resolve('./src/lib/features'),
+      $ui: path.resolve('./src/lib/components/ui'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true, // If using globals like describe, it, expect
