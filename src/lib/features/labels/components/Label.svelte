5f6234faf611a19d665e3674b02c619a30b7f49e
Ger Teck <changerteck@gmail.com>
2026-06-29 14:54:13 +0800

    Fix beta version fetching bug and styling issues (#273)
    fix: conditionally load insertions and deduplicate stylesheet injections in shadow DOM components

--- src/lib/features/labels/components/Label.svelte
+++ src/lib/features/labels/components/Label.svelte
@@ -45,8 +45,8 @@
 {#if labelDef?.value || hasSlotContent}
   {#if labelDef?.value}
     <!-- Inject global stylesheets to support icons (FontAwesome, etc.) inside Shadow DOM -->
-    {#each Array.from(document.querySelectorAll('link[rel="stylesheet"]')) as link ((link as HTMLLinkElement).href)}
-      <link rel="stylesheet" href={(link as HTMLLinkElement).href} />
+    {#each Array.from(new Set(Array.from(document.querySelectorAll('link[rel="stylesheet"]')).map(l => (l as HTMLLinkElement).href))) as href (href)}
+      <link rel="stylesheet" {href} />
     {/each}
   {/if}
   <span class="cv-label" style:background={bgColor} style:color={textColor}>
